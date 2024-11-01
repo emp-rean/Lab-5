b88fd1a40480bcb5373e3f8d54a68ac27e4bd598
emp-rean <spforimport@gmail.com>
2024-11-01 21:07:26 +0200

    54

--- Lab-5.cpp
+++ Lab-5.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
 
-using std::cout;
 using std::cin;
-//using std::cout;
+using std::cout;
+using std::endl;
 
-//void prog1() {
-
-//}
+void prog1() {
+    int n;
+    cout << "enter n: ";
+    cin >> n;
+    int i;
+    unsigned long long mult = 1;
+    int max = 0;
+    int max_number;
+    for (i = 0; i < n; i++) {
+        if(i % 5 == 0)
+            continue;
+            if (max < i)
+                max = i;
+        max_number = max + 1;
+        mult *= i;
+    }
+    cout << "mult = " << mult << endl;
+    cout << "max = " << max << endl;
+    cout << "max number = " << max_number << endl;
+}
 
 void prog2() {
     int mult = 1;
     int digit = 0;
     int N;
+    cout << "enter N;" << endl; 
     cin >> N;
     while (N > 0) {
         digit = N % 10;
@@ -23,6 +41,6 @@ void prog2() {
 }
 
 int main() {
-//prog1;
-prog2;
+    prog1();
+    prog2();
 }
